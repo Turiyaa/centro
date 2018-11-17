8f994702560e9507bd0f80c98c260be5da584d1a
BathSalts <44585723+BathSalts@users.noreply.github.com>
2018-11-17 11:48:57 -0500

    Add files via upload

--- src/main/java/com/sunyoswego/centro/App.java
+++ src/main/java/com/sunyoswego/centro/App.java
@@ -1,25 +1,38 @@
 package com.sunyoswego.centro;
 
 import com.sunyoswego.jdbc.MySqlConnection;
+import com.sunyoswego.entity.BusSchedule;
+import com.sunyoswego.entity.Prediction;
+import com.sunyoswego.entity.Route;
+import com.sunyoswego.entity.Schedule;
+import com.sunyoswego.entity.Stop;
+import com.sunyoswego.entity.Vehicle;
+import com.sunyoswego.dao.CentroDao;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.util.ArrayList;
 
 public class App extends Application
 {
     public static void main( String[] args )
     {
+    	CentroDao dao = new CentroDao();
+		ArrayList<Vehicle> v = dao.getVehicles();
+		ArrayList<Stop> s = dao.getStops();
+		ArrayList<Route> r = dao.getRoute();
+		ArrayList<Prediction> p = dao.getPrediction();
+		ArrayList<BusSchedule> b = dao.getBusSchedule();
     	MySqlConnection.Connection();
     	launch(args);
-    	System.out.println(helloCentro());
-    }
-    
-    public static String helloCentro() {
-    	String sayHello = "Hello Centro";
-    	return sayHello;
+    	v.get(0).displayInfo();
+    	s.get(0).displayInfo();
+    	r.get(0).displayInfo();
+    	p.get(0).displayInfo();
+    	b.get(0).displayInfo();
     }
 
 	@Override
